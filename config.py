cf73aa39f62a41eb770661adc999dd474a7dd47f
Yebin Ahn <yebin@Yebins-MacBook.local>
2025-06-01 21:45:53 +0900

    0601 update - 6

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Ayebin520414#@127.0.0.1:3306/commitme'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:yebin@127.0.0.1:3306/commitme'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
